295f1a7ec8abf9a17e25f34a8216091db5523857
voronov-nikita <114691383+voronov-nikita@users.noreply.github.com>
2022-10-17 20:03:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-a = [353, 22, 222]
+a = [4, 4]
 if len(a) == 0:
     print(None)
 elif len(a) == 1:
     print(a[0])
 else:
-    max_elem = None
-    prev_max_elem = None
-    for elem in a:
-        if max_elem is None or elem > max_elem:
-            prev_max_elem = max_elem
-            max_elem = elem
-        elif prev_max_elem is None or elem > prev_max_elem:
-            prev_max_elem = elem
-print(prev_max_elem)
+    m = None  # минимальное
+    n = None  # предминимальное
+    g = None  # пред-предминимальное
+    for i in a:
+        if i != m and i != n and i != g:    #эллемент не встречался в списке ранее
+            if m is None or i < m:
+                g = n
+                n = m
+                m = i
+            elif n is None or i < n:
+                g = n
+                n = i
+            elif g is None or i < g:
+                g = i
+    print(g)
